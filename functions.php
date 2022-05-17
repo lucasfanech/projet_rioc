da175426c0b71d0605dc204fba0bbc213bb4fcf1
lucas.fanech <fanech.lucas@gmail.com>
2022-05-17 16:00:18 +0200

    user quasi fini

--- functions.php
+++ functions.php
@@ -85,17 +85,18 @@ function getWaitingList($sessionId){
 // Fonction stateUser
 function stateUser($sessionId, $userId){
     include('db.php');
-    $sql = "SELECT * FROM `waiting_line` WHERE processing = 0 AND session_id = 22 AND user_id = 1 ORDER BY waiting_time;";
+    $sql = "SELECT call_type FROM `waiting_line` WHERE processing = 0 AND session_id = ".$sessionId." AND user_id = ".$userId." ORDER BY waiting_time;";
     if (!empty($connection)) {
         $result = mysqli_query($connection, $sql);
     }
 
     if($result){
-        if (isset($result->fetch_row()[4])){
-            if ($result->fetch_row()[4] == 0){
+        $row = mysqli_fetch_row($result);
+        if (isset($row[0])){
+            if ($row[0] == 0){
                 return 1;
             }
-            else if( $result->fetch_row()[4] == 1){
+            else if( $row[0] == 1){
                 return 2;
             }
         }
@@ -108,5 +109,40 @@ function stateUser($sessionId, $userId){
         return mysqli_error($connection);
     }
     // return | en appel = 1, en en verif = 2, rien = 0
+}
+
+// Fonction processButton | appel/verif/cancel
+function processButton($num, $session, $userId){
+    include('db.php');
+    // Set the new timezone
+    date_default_timezone_set('Europe/Paris');
+    $date = date('Y-m-d H:i:s');
+    if ($num == 1){
+        $sql = "INSERT INTO `waiting_line` (`id_waiting`, `session_id`, `user_id`, `waiting_time`, `call_type`, `processing`) VALUES (NULL, '".$session."', '".$userId."', '".$date."', '0', '0');";
+
+    }else if ($num == 2){
+        $sql = "INSERT INTO `waiting_line` (`id_waiting`, `session_id`, `user_id`, `waiting_time`, `call_type`, `processing`) VALUES (NULL, '".$session."', '".$userId."', '".$date."', '1', '0');";
+
+    }else if ($num == 0){
+        // recuperation de l'id de la dernière entrée de l'utilisateur
+        $sql = "SELECT id_waiting FROM `waiting_line` WHERE user_id = ".$userId." AND processing = 0 AND session_id = ".$session.";";
+        if (!empty($connection)) {
+            $result = mysqli_query($connection, $sql);
+        }
+
+        if($result){
+            $row = mysqli_fetch_row($result);
+            $entryId = $row[0];
+        }
+        // suppression
+        $sql = "UPDATE `waiting_line` SET `processing` = '1' WHERE `waiting_line`.`id_waiting` = ".$entryId.";";
+    }
+    if (!empty($connection)) {
+        if ($connection->query($sql) === TRUE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 ?>
--- simulate.php
+++ simulate.php
@@ -1,7 +1,12 @@
 <?php
+include('functions.php');
+$activeSession = getSession();
+
+
+
 // check numéro de table
-if (isset($_POST['idTable'])){
-    $idTable = $_POST['idTable'];
+if (isset($_POST['table'])){
+    $idTable = $_POST['table'];
 }
 
 // Numéro de table
@@ -10,26 +15,96 @@ if (!isset($idTable)){
     ?>
     <form method="POST" action="simulate.php">
 
-        <input type="text" id="simulate" name="simulate" placeholder="Numero de table" required>
+        <input type="text" id="table" name="table" placeholder="Numero de table" required>
         <button type="submit" name="idTable" value="call" >Valider</button>
     </form>
 <?php
 }
 else{
     // Si on a défini la table
+    //Traitement Formulaire:
+    // Envoi formulaire Appel/verif/cancel
+    if (isset($_POST['call'])){
+        if (processButton(1, $activeSession[0], $idTable )){
+            echo "Requête envoyée !<br>";
+        }else{
+            echo "erreur !<br>";
+        }
+    }
+    else if(isset($_POST['verify'])){
+        if(processButton(2, $activeSession[0], $idTable )){
+            echo "Requête envoyée !<br>";
+        }else{
+            echo "erreur !<br>";
+        }
+    }
+    else if(isset($_POST['cancel'])){
+        if(processButton(0, $activeSession[0], $idTable )){
+            echo "Requête annulée !<br>";
+        }
+        else{
+            echo "erreur !<br>";
+        }
+    }else{
+
+    }
+
 
     // Appel DB pour vérifier si le bouton est déjà activé
 
+    $stateUser = stateUser($activeSession[0],$idTable);
+    if ($stateUser == 1){
+        echo "Table n° ".$idTable." - Vous avez appelé l'animateur pour une question.";
+    }
+    else if ($stateUser == 2){
+        echo "Table n° ".$idTable." - Vous avez appelé l'animateur pour une vérification.";
+    }
+    else if ($stateUser == 0){
+        echo "Table n° ".$idTable." - Vous pouvez cliquer.";
+    }
 
-    ?>
-    <form method="POST" action="simulate.php">
+    if ($stateUser == 1){
+        // Appel
+        ?>
+        <form method="POST" action="simulate.php">
 
-        <input id="idTable" name="idTable" type="hidden" value="<?php echo $idTable ?>">
-        <button type="submit" name="call" value="call" >Bouton appel (0)</button>
-        <button type="submit" name="verify" value="verify" >Bouton verif (1)</button>
-        <button type="submit" name="cancel" value="cancel" disabled >Annuler</button>
-    </form>
-<?php
+            <input id="table" name="table" type="hidden" value="<?php echo $idTable ?>">
+            <button type="submit" name="call" value="call" disabled>Bouton appel (0)</button>
+            <button type="submit" name="verify" value="verify" disabled>Bouton verif (1)</button>
+            <button type="submit" name="cancel" value="cancel" >Annuler</button>
+        </form>
+        <?php
+
+    }
+    else if ($stateUser == 2){
+        // Verification
+        ?>
+        <form method="POST" action="simulate.php">
+
+            <input id="table" name="table" type="hidden" value="<?php echo $idTable ?>">
+            <button type="submit" name="call" value="call" disabled>Bouton appel (0)</button>
+            <button type="submit" name="verify" value="verify" disabled>Bouton verif (1)</button>
+            <button type="submit" name="cancel" value="cancel" >Annuler</button>
+        </form>
+        <?php
+
+    }
+    else {
+        // Rien
+        ?>
+        <form method="POST" action="simulate.php">
+
+            <input id="table" name="table" type="hidden" value="<?php echo $idTable ?>">
+            <button type="submit" name="call" value="call" >Bouton appel (0)</button>
+            <button type="submit" name="verify" value="verify" >Bouton verif (1)</button>
+            <button type="submit" name="cancel" value="cancel" disabled >Annuler</button>
+        </form>
+        <?php
+
+    }
+
+
+
 }
 
 
